a5caf5e647156f47f57d303281d35f56c4d86bbf
Dhione Farias <dhionefarias@hotmail.com>
2019-07-04 20:46:10 -0300

    CRUD Scaffolding

---
+++ ProjetoFinalSalesWebMvc/Data/ProjetoFinalSalesWebMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoFinalSalesWebMvc.Models
+{
+    public class ProjetoFinalSalesWebMvcContext : DbContext
+    {
+        public ProjetoFinalSalesWebMvcContext (DbContextOptions<ProjetoFinalSalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoFinalSalesWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- ProjetoFinalSalesWebMvc/Startup.cs
+++ ProjetoFinalSalesWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ProjetoFinalSalesWebMvc.Models;
 
 namespace ProjetoFinalSalesWebMvc
 {
@@ -33,6 +35,9 @@ namespace ProjetoFinalSalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<ProjetoFinalSalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoFinalSalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
